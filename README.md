1c6798694f955034e625c579fccd877e9746c23e
stoleon <stoleon@sogec-marketing.fr>
2023-12-25 01:08:55 +0100

    v0.04.1 afficher le joueur selectioner

--- README.md
+++ README.md
@@ -2,4 +2,8 @@ Projet uniquement dans le but de s'entrainer avec les tableau en LUA
 Poursuivre l'ajout de joueur dans l'équipe
 Reprendre entre la ligne 89 et 82 -> ce code est dans la tour à ré-intégrée dans le code githubs
 
-oublie de push donc création du terrain en parrellèle 
+oublie de push donc tâche effectué en parrellèle 
+afficher le joueur sélectionné via un cercle 
+
+Correction à apporté 
+remplacer la taille du tableau statique en dynamique, la commande est '#'devant la variable contenant le tableau
--- nameTable.lua
+++ nameTable.lua
@@ -6,30 +6,50 @@
 -- version: 0.1
 -- script:  lua
 
-x=0
+x=4 -- variable mode
 y=2
+i=1
+j=1
+z1=150 -- deplacement terrain test
+z2=50 -- variable move terrain te
 
-strJoueur =5+1
+
+P1={
+ {"Mbappe",10,"ATT",150,50},
+	{"Giroud",9,"ATT",140,100},
+	{"Dembele",11,"ATT",130,100},
+	{"Griezmann",7,"MLO",120,100},
+	{"Pogba",6,"MLO",110,100},
+	{"Kante",8,"MLD",100,100},
+	{"Pavard",3,"DEF",90,100},
+	{"Varanne",2,"DEF",80,100},
+	{"Umtiti",5,"DEF",70,100},
+	{"L.Hernendez",4,"DEF",60,100},
+	{"Loris",1,"GK",50,100}
+}
+
+strJoueur =6+1 -- taille du tableau 1/2
 joueurs = {
 	{"Mbappe",10,"ATT"},-- ne pas toucher
 	{"Mbappe",10,"ATT"},
 	{"Griezmann",7,"MLO"},
+	{"Giroud",9,"ATT"},
 	{"Dembele",11,"ATT"},
 	{"L.Hernendez",4,"DEF"},
 	{"L.Hernendez",4,"DEF"}-- ne pas toucher
 }	
 	
 function cadreData(Theme,Mode,pX,pY,pW,pAx,tX,Txt,strTxt)
- if Mode >= 4 then
- 	Mode = 3
-  x= 3
+ if Mode>=9 then
+ 	Mode=8
+  x=8
  elseif Mode <= -1 then 
  	Mode = 0
   x= 0
  end
  
  if y >= strTxt then
- 	y = 5
+ 	y = 6 -- taille du tableau 2/2
  elseif y <= 1 then 
  	y = 2
  end
@@ -58,18 +78,67 @@ end
 
 function TIC()
 
-	if btnp(2) then x=x-1 end
-	if btnp(3) then x=x+1 end
-
+	-- commande 
+	  -- select Mode
+	if keyp(23) then
+	  x=x-1
+	elseif keyp(24) then
+	  x=x+1
+	end
+	
+   -- select joueur
+ if x==1 then
+		if btnp(2) then x=x-1 end
+		if btnp(3) then x=x+1 end
+	end
+	
+	  -- P1 deplacement joueur
+	if x==3 then
+		if btn(0) then P1[i][5]=P1[i][5]-0.25 end
+		if btn(1) then P1[i][5]=P1[i][5]+0.25 end
+		if btn(2) then P1[i][4]=P1[i][4]-0.25 end
+		if btn(3) then P1[i][4]=P1[i][4]+0.25 end
+	end
+	
+   -- P1 change joueur
+ if x==4 then
+		if btnp(2) then i=i-1 end
+		if btnp(3) then i=i+1 end
+		 if i>=12 then
+		 	i=11
+		 elseif i<=0 then 
+		  i=1
+		 end
+	end
+	
+   -- P2 change joueur
+ if x==5 then
+		if btnp(2) then j=j-1 end
+		if btnp(3) then j=j+1 end
+	end
+	
 	cls(0)
 	
 	map()
 	-- debug
-	print(x,10,10,12)
-	print(y,10,20,12)
+	print(x,10,10,12) -- Mode Debug
+	print(y,10,20,12) -- Index Player
+	print(i,10,50,9) -- Index Player 1
+	print(j,220,50,2) -- Index Player 2
 	
-	cadreData(1,x,20,5,40,35,35,joueurs[y][2],strJoueur)
-	cadreData(2,x,90,5,90,130,105,joueurs[y][1],strJoueur)
-	cadreData(1,x,200,5,40,35,212,joueurs[y][3],strJoueur)		
+	-- joueur sur le terrain
+	 -- Equipe P1
+	for k=1, #P1 do
+	 print(P1[k][2],P1[k][4],P1[k][5],9)
+	end
+  -- Controle P1
+ if i==P1[i][2] then 
+  circb(P1[i][4],P1[i][5],5,12)
+	end
+  circb(P1[1][4],P1[1][5],5,12)
+	
+	cadreData(-2,x,20,5,40,35,35,joueurs[y][2],strJoueur)
+	cadreData(1,x,90,5,90,130,105,joueurs[y][1],strJoueur)
+	cadreData(-2,x,200,5,40,35,212,joueurs[y][3],strJoueur)		
 	
 end

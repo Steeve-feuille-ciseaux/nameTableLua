18bd98ceb060ce77d63a143c907592fded39c493
Steeve-feuille-ciseaux <libertybanshee1@gmail.com>
2023-12-26 23:56:22 +0100

    v0.05.1 Ajout second équipe

--- README.md
+++ README.md
@@ -3,9 +3,9 @@ Poursuivre l'ajout de joueur dans l'équipe
 Reprendre entre la ligne 89 et 82 -> ce code est dans la tour à ré-intégrée dans le code githubs
 
 oublie de push donc tâche effectué en parrellèle 
-afficher le joueur sélectionné via un cercle 
+Ajout second équip + selection 
 
 Correction à apporté 
 remplacer la taille du tableau statique en dynamique, la commande est '#'devant la variable contenant le tableau
 
-v0.04.1
+v0.05.1
--- nameTable.lua
+++ nameTable.lua
@@ -6,37 +6,40 @@
 -- version: 0.1
 -- script:  lua
 
-x=4 -- variable mode
+x=2 -- variable mode
 y=2
 i=1
 j=1
 z1=150 -- deplacement terrain test
 z2=50 -- variable move terrain te
 
-
 P1={
- {"Mbappe",10,"ATT",150,50},
-	{"Giroud",9,"ATT",140,100},
-	{"Dembele",11,"ATT",130,100},
-	{"Griezmann",7,"MLO",120,100},
-	{"Pogba",6,"MLO",110,100},
-	{"Kante",8,"MLD",100,100},
-	{"Pavard",3,"DEF",90,100},
-	{"Varanne",2,"DEF",80,100},
-	{"Umtiti",5,"DEF",70,100},
-	{"L.Hernendez",4,"DEF",60,100},
-	{"Loris",1,"GK",50,100}
+	{"Lloris",1,"GK",40,63},
+	{"Pavard",2,"DEF",65,30},
+	{"Varane",4,"DEF",57,52.75},
+	{"Umtiti",5,"DEF",57.25,72},
+	{"L.Hernendez",21,"DEF",65.5,90},
+	{"Pogba",6,"MLO",85.75,43.75},
+	{"Kante",13,"MLD",75.75,60.25},
+	{"Tolisso",12,"MLO",84.25,77},
+	{"Griezmann",7,"ATT",100,33},
+ {"Mbappe",10,"ATT",104,62.5},
+	{"Dembele",11,"ATT",100,91.25}
 }
 
 strJoueur =6+1 -- taille du tableau 1/2
-joueurs = {
-	{"Mbappe",10,"ATT"},-- ne pas toucher
-	{"Mbappe",10,"ATT"},
-	{"Griezmann",7,"MLO"},
-	{"Giroud",9,"ATT"},
-	{"Dembele",11,"ATT"},
-	{"L.Hernendez",4,"DEF"},
-	{"L.Hernendez",4,"DEF"}-- ne pas toucher
+P2 = {
+	{"Joueur1",1,"GK",190,63},
+	{"Joueur2",2,"DEF",160,30},
+	{"Joueur3",3,"DEF",170,52.75},
+	{"Joueur4",4,"DEF",170,72},
+	{"Joueur5",5,"DEF",160,90},
+	{"Joueur6",6,"MLO",145,43.75},
+	{"Joueur7",13,"MLD",150,60.25},
+	{"Joueur8",10,"MLO",145,77},
+	{"Joueur9",7,"ATT",128,33},
+ {"Joueur10",9,"ATT",125,62.5},
+	{"Joueur11",11,"ATT",123,91.25}
 }	
 	
 function cadreData(Theme,Mode,pX,pY,pW,pAx,tX,Txt,strTxt)
@@ -72,7 +75,7 @@ function cadreData(Theme,Mode,pX,pY,pW,pAx,tX,Txt,strTxt)
 	spr(36,pAx,Down)
 	end
 	tY=pY+10
-	rectb(pX,pY,pW,25,12)
+	rectb(pX,pY,pW,22,12)
 	print(Txt,tX,tY,12)
 end
 
@@ -80,10 +83,12 @@ function TIC()
 
 	-- commande 
 	  -- select Mode
-	if keyp(23) then
-	  x=x-1
-	elseif keyp(24) then
-	  x=x+1
+	if keyp(28) then
+	  x=1
+	elseif keyp(29) then
+	  x=2
+	elseif keyp(30) then
+	  x=3
 	end
 	
    -- select joueur
@@ -93,28 +98,35 @@ function TIC()
 	end
 	
 	  -- P1 deplacement joueur
-	if x==3 then
+	if x==2 then
+   -- P1 change joueur
+		if keyp(23) then i=i-1 end
+		if keyp(24) then i=i+1 end
+	 if i>=12 then
+	 	i=11
+	 elseif i<=0 then 
+	  i=1
+	 end
 		if btn(0) then P1[i][5]=P1[i][5]-0.25 end
 		if btn(1) then P1[i][5]=P1[i][5]+0.25 end
 		if btn(2) then P1[i][4]=P1[i][4]-0.25 end
 		if btn(3) then P1[i][4]=P1[i][4]+0.25 end
 	end
 	
-   -- P1 change joueur
- if x==4 then
-		if btnp(2) then i=i-1 end
-		if btnp(3) then i=i+1 end
-		 if i>=12 then
-		 	i=11
-		 elseif i<=0 then 
-		  i=1
-		 end
-	end
-	
    -- P2 change joueur
- if x==5 then
-		if btnp(2) then j=j-1 end
-		if btnp(3) then j=j+1 end
+ if x==3 then
+   -- P2 change joueur
+		if keyp(23) then j=j-1 end
+		if keyp(24) then j=j+1 end
+	 if j>=12 then
+	 	j=11
+	 elseif j<=0 then 
+	  j=1
+	 end
+		if btn(0) then P2[j][5]=P2[j][5]-0.25 end
+		if btn(1) then P2[j][5]=P2[j][5]+0.25 end
+		if btn(2) then P2[j][4]=P2[j][4]-0.25 end
+		if btn(3) then P2[j][4]=P2[j][4]+0.25 end
 	end
 	
 	cls(0)
@@ -124,7 +136,11 @@ function TIC()
 	print(x,10,10,12) -- Mode Debug
 	print(y,10,20,12) -- Index Player
 	print(i,10,50,9) -- Index Player 1
+	print("X : "..P1[i][4],0,60,9) -- Px index P1
+	print("Y : "..P1[i][5],0,70,9) -- Px index P1
 	print(j,220,50,2) -- Index Player 2
+	print("X : "..P2[j][4],200,60,2) -- Px index P2
+	print("Y : "..P2[j][5],200,70,2) -- Px index P2
 	
 	-- joueur sur le terrain
 	 -- Equipe P1
@@ -132,13 +148,21 @@ function TIC()
 	 print(P1[k][2],P1[k][4],P1[k][5],9)
 	end
   -- Controle P1
- if i==P1[i][2] then 
-  circb(P1[i][4],P1[i][5],5,12)
+ circb(P1[i][4]+2,P1[i][5]+2,5,12)
+	
+	cadreData(-2,x,20,0,40,35,35,P1[i][2],strJoueur)
+	cadreData(1,x,90,0,90,130,105,P1[i][1],strJoueur)
+	cadreData(-2,x,200,0,40,35,212,P1[i][3],strJoueur)	
+	
+	 -- Equipe P2
+	for k=1, #P2 do
+	 print(P2[k][2],P2[k][4],P2[k][5],2)
 	end
-  circb(P1[1][4],P1[1][5],5,12)
+  -- Controle P2
+ circb(P2[j][4]+2,P2[j][5]+2,5,12)
 	
-	cadreData(-2,x,20,5,40,35,35,joueurs[y][2],strJoueur)
-	cadreData(1,x,90,5,90,130,105,joueurs[y][1],strJoueur)
-	cadreData(-2,x,200,5,40,35,212,joueurs[y][3],strJoueur)		
-	
+	cadreData(-2,x,20,110,40,35,35,P2[j][2],strJoueur)
+	cadreData(1,x,90,110,90,130,105,P2[j][1],strJoueur)
+	cadreData(-2,x,200,110,40,35,212,P2[j][3],strJoueur)		
+		
 end
